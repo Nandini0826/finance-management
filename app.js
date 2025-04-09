f363e97a21f4e78e1a58638143731534937fbcf3
Nandini0826 <nandini.nd17@gmail.com>
2025-04-09 20:23:17 +0530

    added employee login and registratin routes

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ const expressSession = require("express-session");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const PORT = process.env.PORT;
+const employeeRouter = require("./routes/employee-router")
 
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get("/", function(req, res){
+ console.log("working");
+})
+app.use("/employee", employeeRouter);
 
-app.set("view engine", "ejs");
 
-app.listen(PORT, (err)=>{
-   if(err) console.log(err);
-   else {
-      console.log("Server is running at port:", PORT);
-   }
-});
+
+app.listen(3000);
